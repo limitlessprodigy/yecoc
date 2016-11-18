2b7f5333cf380b1e79307fe71f153019a9fa125a
Marc Lewis <limitlessprodigy@yahoo.com>
2016-11-18 16:24:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,2 @@
 <?php header( 'Location: /index.html' ) ;  ?>
-<?php
-if(!isset($_POST['submit'])){
-  echo "Nope! You need to submit the form before accessing the next page.";
-}
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
 
-if(empty($name)||empty($email)||empty($message)){
-  echo "You're missing something, try again!";
-  exit;
-}
-
-$to = cartersanders15@gmail.com;
-$subject = "Contact form - " . $name;
-$body = "$name has completed the contact form with the email '$email'.\n\r" .
-  "Message: \n\r" . "$message";
-
-mail($to,$subject,$body,$email);
-
-echo "Thank you for contacting us!";
-?>
